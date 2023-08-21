3f9af0e05737cbf55c991de3ef42a5f89cba7539
oddyamill <43600905+oddyamill@users.noreply.github.com>
2023-08-21 15:45:12 +0700

    так лучше .

--- src/client.js
+++ src/client.js
@@ -2,6 +2,6 @@ import { Client } from 'discord-rpc';
 import { clientId } from './constants.js';
 
 const client = new Client({ transport: 'ipc' });
-await client.login({ clientId: clientId });
+await client.login({ clientId });
 
 export { client };
